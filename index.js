60044428a6e9880b01d3509fa54d56aedfe13a36
Ambar Khare <ambar.khare24@gmail.com>
2022-08-02 09:19:00 -0700

    commit tree 96c12e2dd8646e38513aead0d1a4f75f1b82edbf

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'ak'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = 'x'
+}
+
+const leastFavoriteCustomer = 'y'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'z'
+}
+
